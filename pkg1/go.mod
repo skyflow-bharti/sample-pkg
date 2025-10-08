3f391e8c49a1d7668dbf3b4c140276dc7f28d384
skyflow-bharti <bharti.sagar@skyflow.com>
2025-10-08 11:29:38 +0530

    Revert "test"
    This reverts commit ef26028f849c5c7b49ebbb86d2351c46c6ac68c5.

---
+++ pkg1/go.mod
@@ -0,0 +1,5 @@
+module github.com/skyflow-bharti/sample-pkg/pkg1
+
+go 1.20
+
+require github.com/skyflow-bharti/sample-pkg/pkg2 v0.0.0-20250121161147-6a605837f2ac
---
+++ pkg1/index.go
@@ -0,0 +1,13 @@
+package pkg1
+
+import (
+	"fmt"
+
+	"github.com/skyflow-bharti/sample-pkg/pkg2"
+)
+
+// Hello returns a greeting message
+func Hello(name string) string {
+	fmt.Println("here is my SDK Name", pkg2.SDK)
+	return fmt.Sprintf("Hello from pkg1, %s!", name)
+}
